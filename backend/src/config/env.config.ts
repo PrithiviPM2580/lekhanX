b97c95af0706b1eaed6393c82a8656bc04676ae4
PrithiviPM2580 <prithivipemimagar>
2025-11-20 12:37:47 +0545

    feat: update environment configuration to include admin and JWT settings

--- backend/src/config/env.config.ts
+++ backend/src/config/env.config.ts
@@ -9,13 +9,18 @@ import envSchema from "@/validators/env.validator.js";
 // validateEnv{} — Validates environment variables
 // ------------------------------------------------------
 const envConfig = {
-	PORT: process.env.PORT,
-	NODE_ENV: process.env.NODE_ENV,
-	LOG_LEVEL: process.env.LOG_LEVEL,
-	DB_URL: process.env.DB_URL,
-	DB_NAME: process.env.DB_NAME,
-	APP_NAME: process.env.APP_NAME,
-	APP_VERSION: process.env.APP_VERSION,
+  PORT: process.env.PORT,
+  NODE_ENV: process.env.NODE_ENV,
+  LOG_LEVEL: process.env.LOG_LEVEL,
+  DB_URL: process.env.DB_URL,
+  DB_NAME: process.env.DB_NAME,
+  APP_NAME: process.env.APP_NAME,
+  APP_VERSION: process.env.APP_VERSION,
+  ADMIN_EMAIL: process.env.ADMIN_EMAIL,
+  JWT_ACCESS_TOKEN_SECRET: process.env.JWT_ACCESS_TOKEN_SECRET,
+  JWT_REFRESH_TOKEN_SECRET: process.env.JWT_REFRESH_TOKEN_SECRET,
+  JWT_ACCESS_TOKEN_EXPIRATION: process.env.JWT_ACCESS_TOKEN_EXPIRATION,
+  JWT_REFRESH_TOKEN_EXPIRATION: process.env.JWT_REFRESH_TOKEN_EXPIRATION,
 };
 
 // Validate and export the environment configuration
